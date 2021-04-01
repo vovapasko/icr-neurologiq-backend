6ca5424d6c9a6e2045786ac491cff92ccece3864
Yaroslav Artemenko <Yaroslav@MBPvonNurologIQ.mshome.net>
2021-04-01 19:11:06 +0200

    changed on new coordinates

--- ai/config.py
+++ ai/config.py
@@ -16,13 +16,15 @@ OCRLocation = namedtuple("Location", ["id", "bbox", "filter_keywords"])
 
 OCR_LOCATIONS = [
     # up left
-    # OCRLocation("steuernummer", (118, 270, 440, 53), ["steuernummer", "bzw", "umsatzsteuer", "identifikationsnummer"]),
-    # OCRLocation("rechnungsnummer", (118, 351, 440, 53), ["rechnungsnummer", "gemaess", "14", "abs", "4", "nr", "ustg"]),
+    OCRLocation("steuernummer", (118, 270, 440, 53), ["steuernummer", "bzw", "umsatzsteuer", "identifikationsnummer"]),
+    OCRLocation("rechnungsnummer", (118, 351, 440, 53), ["rechnungsnummer", "gemaess", "14", "abs", "4", "nr", "ustg"]),
 
     # up right
     OCRLocation("psd_kontonummer", (1075, 271, 441, 53), ["psd", "kontonummer"]),
 
     # kontoinhaber block
+    OCRLocation("geschlecht_frau", (1117, 510, 40, 30), []),
+    OCRLocation("geschlecht_mann", (1229, 510, 40, 30), []),
     OCRLocation("kontoinhar_vorname_name", (839, 552, 669, 49), ["vorname", "name"]),
     OCRLocation("kontoinhaber_strasse", (839, 619, 669, 49), ["strasse", "hausnummer"]),
     OCRLocation("kontoinhaber_plz_ort", (839, 690, 669, 49), ["plz", "ort"]),
@@ -39,34 +41,34 @@ OCR_LOCATIONS = [
     OCRLocation("auftrag_iban", (209, 1347, 455, 49), ["iban"]),
 
     # SEPA-Lastschrift/Umbuchung
-    # OCRLocation("sepa_betrag_girokonto", (128, 1571, 234, 47), ["betrag"]),
+    OCRLocation("sepa_betrag_girokonto", (128, 1571, 234, 47), ["betrag"]),
     OCRLocation("sepa_betrag_psd_bank_konto", (128, 1641, 234, 47), ["betrag"]), #nicht lesbar
     OCRLocation("sepa_iban", (760, 1639, 513, 50), ["iban"]),
-    # OCRLocation("sepa_unterschrift", (128, 1705, 474, 46), ["unterschrift", "des", "kontoinhabers", "sofern", "nicht",
-    #                                                         "auftaggeber"]),
-    # OCRLocation("sepa_name_vorname", (740, 1708, 596, 44), ["name", "vorname", "weiterer", "auftraggeber"]),
+    OCRLocation("sepa_unterschrift", (128, 1705, 474, 46), ["unterschrift", "des", "kontoinhabers", "sofern", "nicht",
+                                                            "auftaggeber"]),
+    OCRLocation("sepa_name_vorname", (740, 1708, 596, 44), ["name", "vorname", "weiterer", "auftraggeber"]),
 
     # Ort, Datum, Unterschriften
-    # OCRLocation("ort_datum", (136, 1802, 432, 54), ["ort", "datum"]),
-    # OCRLocation("unterschrift_beitretender_mitlied", (615, 1800, 919, 57), ["unterschrift", "beitretender", "mitlied"]),
+    OCRLocation("ort_datum", (136, 1802, 432, 54), ["ort", "datum"]),
+    OCRLocation("unterschrift_beitretender_mitlied", (615, 1800, 919, 57), ["unterschrift", "beitretender", "mitlied"]),
 
     # Mitglied minderjaerig
-    # OCRLocation("mitglied_ort_datum", (137, 1919, 680, 55), ["ort", "datum"]),
-    # OCRLocation("mitglied_unterschriften_bei_Minderjaerigen", (837, 1924, 694, 55), ["unterschriften", "bei",
-    #                                                                                  "Minderjaerigen", "von"
-    #                                                                                                    "allen",
-    #                                                                                  "gesetzlichen",
-    #                                                                                  "vertretern"]),
+    OCRLocation("mitglied_ort_datum", (137, 1919, 680, 55), ["ort", "datum"]),
+    OCRLocation("mitglied_unterschriften_bei_Minderjaerigen", (837, 1924, 694, 55), ["unterschriften", "bei",
+                                                                                     "Minderjaerigen", "von"
+                                                                                                       "allen",
+                                                                                     "gesetzlichen",
+                                                                                     "vertretern"]),
     OCRLocation("psd_formularnummer", (197, 2212, 125, 61), [])
 ]
 
 ORC_second_page = [
     # SEPA-Lastschriftmandat
-    OCRLocation("sepa_lastschriftmandat_identifikationsnummer", (145, 102, 629, 50), ["glaeubireger",
-                                                                                      "identifikationsnummer", "ci",
-                                                                                      "creditor", "identifier"]),
-    OCRLocation("sepa_lastschriftmandat_mandatsreferenz", (805, 101, 732, 52), ["mandatsreferenz", "von",
-                                                                               "der", "bank", "ausgefuellt"]),
+    # OCRLocation("sepa_lastschriftmandat_identifikationsnummer", (145, 102, 629, 50), ["glaeubireger",
+    #                                                                                   "identifikationsnummer", "ci",
+    #                                                                                   "creditor", "identifier"]),
+    # OCRLocation("sepa_lastschriftmandat_mandatsreferenz", (805, 101, 732, 52), ["mandatsreferenz", "von",
+    #                                                                            "der", "bank", "ausgefuellt"]),
     OCRLocation("sepa_lastschriftmandat_kontoinhaber", (137, 340, 1393, 55), ["x", "kontoinhaber", "vorname", "name"]),
     OCRLocation("sepa_lastschriftmandat_anschrift", (137, 415, 1391, 40), ["x", "anschrift", "strasse", "hausnummer",
                                                                            "plz", "ort", "bitte", "angeben", "wenn",
